63c19dd23bdd4561a09613233471e6dd09f12061
vicky <jiajun_work@126.com>
2020-07-24 17:04:29 +0800

    [TDD][vicky] feature:add ParkingBoy.park and Car.

---
+++ src/main/java/com/oocl/cultivation/Car.java
@@ -0,0 +1,10 @@
+package com.oocl.cultivation;
+
+/**
+ * @Author LINVI7
+ * @Date 7/24/2020 4:40 PM
+ * @Version 1.0
+ */
+public class Car {
+    private String carId;
+}
---
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -0,0 +1,12 @@
+package com.oocl.cultivation;
+
+/**
+ * @Author LINVI7
+ * @Date 7/24/2020 4:41 PM
+ * @Version 1.0
+ */
+public class ParkingBoy {
+    public String park(Car car) {
+        return "A001";
+    }
+}
