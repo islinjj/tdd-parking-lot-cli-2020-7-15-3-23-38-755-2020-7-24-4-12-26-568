d35fcb492cdf6120239bffbd20538fd09e5974dc
Vicky Lin <jiajun_work@126.com>
2020-07-25 12:48:48 +0800

    [Parking lot Story1][vicky] feature:add error message with null ticket.

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -22,6 +22,10 @@ public class ParkingLot {
     }
 
     public Car fetch(CarTicket parkingTicket) {
+        if (parkingTicket == null){
+            System.out.print("Unrecognized parking ticket.");
+            return null;
+        }
         Car car = parkedCars.get(parkingTicket);
         parkedCars.remove(parkingTicket);
         return car;
