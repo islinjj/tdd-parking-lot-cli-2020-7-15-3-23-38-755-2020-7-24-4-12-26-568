5b0f807a8b170a394815fe0ec7f77f911d5f4e28
Vicky Lin <jiajun_work@126.com>
2020-07-25 10:32:55 +0800

    [Parking lot Story1][vicky] refactor:modify parameter name.

--- src/test/java/com/oocl/cultivation/test/ParkingTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingTest.java
@@ -24,13 +24,13 @@ class ParkingTest {
     @Test
     void should_return_2_parking_tickets_when_park_given_2_cars() {
         //given
-        Car parkingCarOne = new Car("A001");
-        Car parkingCarTwo = new Car("A002");
+        Car carOne = new Car("A001");
+        Car carTwo = new Car("A002");
         ParkingLot parkingLot = new ParkingLot();
 
         //when
-        CarTicket parkingTicketOne = parkingLot.park(parkingCarOne);
-        CarTicket parkingTicketTwo = parkingLot.park(parkingCarTwo);
+        CarTicket parkingTicketOne = parkingLot.park(carOne);
+        CarTicket parkingTicketTwo = parkingLot.park(carTwo);
 
         Assertions.assertNotNull(parkingTicketOne);
         Assertions.assertNotNull(parkingTicketTwo);
@@ -39,26 +39,26 @@ class ParkingTest {
     @Test
     void should_fetch_1_car_when_fetch_given_1_correct_parking_ticket() {
         //given
-        Car parkingCar = new Car("A001");
+        Car car = new Car("A001");
         ParkingLot parkingLot = new ParkingLot();
-        CarTicket parkingTicket = parkingLot.park(parkingCar);
+        CarTicket parkingTicket = parkingLot.park(car);
 
         //when
         Car fetchCar = parkingLot.fetch(parkingTicket);
 
         //then
         Assertions.assertNotNull(fetchCar);
-        Assertions.assertEquals(parkingCar,fetchCar);
+        Assertions.assertEquals(car,fetchCar);
     }
 
     @Test
     void should_fetch_2_cars_when_fetch_given_2_correct_parking_tickets() {
         //given
-        Car parkingCarOne = new Car("A001");
-        Car parkingCarTwo = new Car("A002");
+        Car carOne = new Car("A001");
+        Car carTwo = new Car("A002");
         ParkingLot parkingLot = new ParkingLot();
-        CarTicket parkingTicketOne = parkingLot.park(parkingCarOne);
-        CarTicket parkingTicketTwo = parkingLot.park(parkingCarTwo);
+        CarTicket parkingTicketOne = parkingLot.park(carOne);
+        CarTicket parkingTicketTwo = parkingLot.park(carTwo);
 
         //when
         Car fetchCarOne = parkingLot.fetch(parkingTicketOne);
@@ -67,33 +67,33 @@ class ParkingTest {
         //then
         Assertions.assertNotNull(fetchCarOne);
         Assertions.assertNotNull(fetchCarTwo);
-        Assertions.assertEquals(parkingCarOne,fetchCarOne);
-        Assertions.assertEquals(parkingCarTwo,fetchCarTwo);
+        Assertions.assertEquals(carOne,fetchCarOne);
+        Assertions.assertEquals(carTwo,fetchCarTwo);
     }
 
     @Test
     void should_fetch_1_correct_car_when_fetch_1_car_from_2_cars_given_2_cars() {
         //given
-        Car parkingCarOne = new Car("A001");
-        Car parkingCarTwo = new Car("A002");
+        Car carOne = new Car("A001");
+        Car carTwo = new Car("A002");
         ParkingLot parkingLot = new ParkingLot();
-        CarTicket parkingTicketOne = parkingLot.park(parkingCarOne);
-        CarTicket parkingTicketTwo = parkingLot.park(parkingCarTwo);
+        CarTicket parkingTicketOne = parkingLot.park(carOne);
+        CarTicket parkingTicketTwo = parkingLot.park(carTwo);
 
         //when
         Car fetchCarOne = parkingLot.fetch(parkingTicketOne);
 
         //then
         Assertions.assertNotNull(fetchCarOne);
-        Assertions.assertEquals(parkingCarOne,fetchCarOne);
+        Assertions.assertEquals(carOne,fetchCarOne);
     }
 
     @Test
     void should_fetch_null_when_fetch_given_no_ticket() {
         //given
-        Car parkingCar = new Car("A001");
+        Car car = new Car("A001");
         ParkingLot parkingLot = new ParkingLot();
-        parkingLot.park(parkingCar);
+        parkingLot.park(car);
 
         //when
         Car fetchCar = parkingLot.fetch(null);
