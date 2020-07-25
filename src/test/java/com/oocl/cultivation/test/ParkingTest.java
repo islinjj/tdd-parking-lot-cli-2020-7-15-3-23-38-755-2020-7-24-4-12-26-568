ba245451c2098951ece97a185d5c05d7fcca5afb
Vicky Lin <jiajun_work@126.com>
2020-07-25 10:02:42 +0800

    [TDD][vicky] refactor:modify all test method name.

--- src/test/java/com/oocl/cultivation/test/ParkingTest.java
+++ src/test/java/com/oocl/cultivation/test/ParkingTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 class ParkingTest {
     @Test
-    void should_return_parking_ticket_when_park_given_a_car() {
+    void should_return_parking_ticket_when_park_given_1_car() {
         //given
         String carId = "A001";
         Car car = new Car(carId);
@@ -22,7 +22,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_return_2_parking_ticket_when_park_given_two_car() {
+    void should_return_2_parking_tickets_when_park_given_2_cars() {
         //given
         Car parkingCarOne = new Car("A001");
         Car parkingCarTwo = new Car("A002");
@@ -37,7 +37,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_fetch_a_car_when_fetch_given_a_correct_parking_ticket() {
+    void should_fetch_1_car_when_fetch_given_1_correct_parking_ticket() {
         //given
         Car parkingCar = new Car("A001");
         ParkingLot parkingLot = new ParkingLot();
@@ -52,7 +52,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_fetch_2_car_when_fetch_given_2_correct_parking_ticket() {
+    void should_fetch_2_cars_when_fetch_given_2_correct_parking_tickets() {
         //given
         Car parkingCarOne = new Car("A001");
         Car parkingCarTwo = new Car("A002");
@@ -72,7 +72,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_fetch_a_correct_car_when_fetch_a_car_from_2_car_given_2_car() {
+    void should_fetch_1_correct_car_when_fetch_1_car_from_2_cars_given_2_cars() {
         //given
         Car parkingCarOne = new Car("A001");
         Car parkingCarTwo = new Car("A002");
@@ -89,7 +89,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_return_null_when_fetch_given_no_ticket() {
+    void should_fetch_null_when_fetch_given_no_ticket() {
         //given
         Car parkingCar = new Car("A001");
         ParkingLot parkingLot = new ParkingLot();
@@ -103,7 +103,7 @@ class ParkingTest {
     }
 
     @Test
-    void should_return_null_when_fetch_given_wrong_ticket() {
+    void should_fetch_null_when_fetch_given_wrong_ticket() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         CarTicket wrongParkingTicket = new CarTicket("xxxx");
