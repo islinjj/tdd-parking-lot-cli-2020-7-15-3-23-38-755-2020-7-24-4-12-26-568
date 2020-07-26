d43c42e14cff8b806ca2109cd9bf24f4a3bfa03d
Vicky Lin <jiajun_work@126.com>
2020-07-26 10:14:28 +0800

    [Parking lot Story5][vicky] refactor:add no car then no ticket.

---
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -0,0 +1,18 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SuperSmartParkingBoy {
+    private List<ParkingLot> parkingLotList;
+
+    public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
+        this.parkingLotList = parkingLotList;
+    }
+
+    public CarTicket park(Car car) {
+        if (car != null){
+
+        }
+        return null;
+    }
+}
